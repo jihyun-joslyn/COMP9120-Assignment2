3c8af16cb2f2de4f0bf9c8924114ca2c1791cbac
jihyun-joslyn <129327085+jihyun-joslyn@users.noreply.github.com>
2023-04-30 17:15:23 +1000

    Add files via upload

--- public.fnMovieSearchMovieListByKeyword.Function.sql
+++ public.fnMovieSearchMovieListByKeyword.Function.sql
@@ -1,7 +1,9 @@
-CREATE or REPLACE FUNCTION fnMovieSearchMovieListByKeyword (searchKeyword varchar, userName varchar) 
+CREATE or REPLACE FUNCTION fnMovieSearchMovieListByKeyword (searchKeyword VARCHAR, userName VARCHAR) 
 RETURNS TABLE (
     MovieID INT,
     MovieTitle VARCHAR(100),
+    Genre VARCHAR(100),
+    Rating VARCHAR(5),
     ReleasedDate DATE,
     Description VARCHAR(500)
 )
@@ -13,53 +15,88 @@ BEGIN
     CREATE TEMP TABLE Result (
         MovieID INT,
         MovieTitle VARCHAR(100),
+        Genre VARCHAR(100),
+        Rating VARCHAR(5),
         ReleasedDate DATE,
         Description VARCHAR(500)
     );
 
-    if (searchKeyword = COALESCE(searchKeyword, '')) then 
+    IF (searchKeyword = COALESCE(searchKeyword, '')) THEN 
         BEGIN
-            insert into Result(MovieID, MovieTitle, ReleasedDate, Description)
-            select * from Movie m where m.Ma
-        END
-    end if;
-
-    select lower(searchKeyword) into searchKeyword;
+            INSERT INTO Result(MovieID, MovieTitle, ReleasedDate, Rating, Description)
+            SELECT m.ID, 
+                m.Title, 
+                m.ReleaseDate, 
+                CAST(m.AVGRating AS VARCHAR(5)), 
+                m.Description 
+            FROM Movie m 
+            WHERE m.ManagedBy =  userName;
+        END;
+    ELSE
+        BEGIN
+            SELECT LOWER(searchKeyword) INTO searchKeyword;
     
-    INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Description)
-    SELECT m.ID,
-            m.Title, 
-            m.ReleaseDate, 
-            m.Description 
-    FROM Movie m 
-    WHERE lower(m.Title) like ('%' || searchKeyword || '%');
+            INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Rating, Description)
+            SELECT m.ID,
+                    m.Title, 
+                    m.ReleaseDate, 
+                    CAST(m.AVGRating AS VARCHAR(5)),
+                    m.Description 
+            FROM Movie m 
+            WHERE LOWER(m.Title) LIKE ('%' || searchKeyword || '%');
 
-    INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Description)
-    SELECT m.ID,
-            m.Title, 
-            m.ReleaseDate, 
-            m.Description 
-    FROM Movie m 
-    WHERE lower(m.Description) like ('%' || searchKeyword || '%');
+            INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Rating, Description)
+            SELECT m.ID,
+                m.Title, 
+                m.ReleaseDate, 
+                CAST(m.AVGRating AS VARCHAR(5)),
+                m.Description 
+            FROM Movie m 
+            WHERE LOWER(m.Description) LIKE ('%' || searchKeyword || '%');
 
-    INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Description)
-    SELECT m.ID,
-            m.Title, 
-            m.ReleaseDate, 
-            m.Description 
-    FROM Movie m 
-    WHERE exists (select 1 from Genre g where (m.PrimaryGenre = g.GenreId or m.SecondaryGenre = g.GenreID) and lower(g.GenreName) like ('%' || searchKeyword || '%') limit 1);
+            INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Rating, Description)
+            SELECT m.ID,
+                    m.Title, 
+                    m.ReleaseDate, 
+                    CAST(m.AVGRating AS VARCHAR(5)),
+                    m.Description 
+            FROM Movie m 
+            WHERE EXISTS (SELECT 1 
+                            FROM Genre g 
+                            WHERE (m.PrimaryGenre = g.GenreId 
+                                    OR m.SecondaryGenre = g.GenreID) 
+                                AND LOWER(g.GenreName) LIKE ('%' || searchKeyword || '%') 
+                            LIMIT 1);
 
-    INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Description)
-    SELECT m.ID,
-            m.Title, 
-            m.ReleaseDate, 
-            m.Description 
+            INSERT INTO Result (MovieID, MovieTitle, ReleasedDate, Rating, Description)
+            SELECT m.ID,
+                m.Title, 
+                m.ReleaseDate, 
+                CAST(m.AVGRating AS VARCHAR(5)),
+                m.Description 
+            FROM Movie m 
+            WHERE EXISTS (SELECT 1 
+                            FROM Staff s 
+                            WHERE s.Login = m.ManagedBy 
+                                AND (LOWER(s.FirstName) LIKE ('%' || searchKeyword || '%') 
+                                    OR LOWER(s.LastName) LIKE ('%' || searchKeyword || '%')) 
+                            LIMIT 1);
+
+        END;
+    END IF;
+
+    UPDATE Result r 
+    SET Genre = (SELECT STRING_AGG(g.GenreName, ', ') 
+                    FROM Genre g 
+                    WHERE m.PrimaryGenre = g.GenreID 
+                        OR m.SecondaryGenre = g.GenreID) 
     FROM Movie m 
-    WHERE exists (select 1 from Staff s where s.Login = m.ManagedBy and (lower(s.FirstName) like ('%' || searchKeyword || '%') or lower(s.LastName) like ('%' || searchKeyword || '%')) limit 1);
+    WHERE r.MovieID = m.ID;
 
     RETURN query (SELECT r.MovieID, 
                             r.MovieTitle, 
+                            r.Genre, 
+                            r.Rating,
                             r.ReleasedDate, 
                             r.Description 
                     FROM Result r 
@@ -70,7 +107,9 @@ END
 $$
 
 
---select * from movie; select * from staff; select * from genre
---select * from fnMovieSearchMovieListByKeyword('iron')
+--SELECT * FROM movie; SELECT * FROM staff; SELECT * FROM genre
+--SELECT * FROM fnMovieSearchMovieListByKeyword('', 'jdavis')
 
---select * from movie where title like '%iron%'
+--SELECT * FROM movie WHERE title LIKE '%iron%'
+
+--drop function fnMovieSearchMovieListByKeyword(VARCHAR, VARCHAR)
